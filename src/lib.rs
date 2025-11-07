68c769453d3d1e17fb8b4d7bb386e270e3e90a64
maulanazh15 <maulanazh15@gmail.com>
2025-11-07 23:47:01 +0700

    minigrep v1

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod test {
     use std::vec;
@@ -21,8 +34,23 @@ mod test {
         let contents = "\
 Rust:
 aman, cepat, produktif.
-Ambil Tiga.";
+Ambil Tiga.
+proDuktor Lakban.";
 
         assert_eq!(vec!["aman, cepat, produktif."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RuSt";
+        let contents = "\
+Rust:
+aman, cepat, produktif.
+Ambil Tiga.
+Trust padaku.";
+        assert_eq!(
+            vec!["Rust:", "Trust padaku."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
